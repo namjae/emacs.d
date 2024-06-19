31e5e5035ebf13ad3cf64f3aff24faba737d8d2f
DalHo Park <airdalho@gmail.com>
2024-06-19 10:08:23 +0900

    trivial changes.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -8,7 +8,8 @@
 (setq system-time-locale "ko_kr.utf-8")
 
 (setq org-agenda-files
-      (append (file-expand-wildcards "g:/My Drive/.org/*.org")
+      (append (file-expand-wildcards "g:/My Drive/.org/*/*.org")
+              (file-expand-wildcards "g:/내 드라이브/.org/*/*.org")
               (file-expand-wildcards "h:/My Drive/.org/*.org")
               (file-expand-wildcards "~/.org-mode/*.org")
               (file-expand-wildcards "~/Documents/journal/*.org")))
@@ -23,8 +24,11 @@
 ;;   ;(org-journal-update-auto-mode-alist)
 ;;   (setq org-journal-date-prefix "#+TITLE: Daily Notes "))
 
+(setq org-journal-dir "g:/내 드라이브/.org/journal")
 (setq org-journal-file-format "%Y%m%d.org")
 (setq org-journal-date-prefix "#+TITLE: Daily Notes ")
+;; from: https://emacs.stackexchange.com/questions/61819/how-can-i-bind-c-c-c-j-to-always-do-org-journal-new-entry
+(global-set-key (kbd "C-c j") 'org-journal-new-entry)
 
 ;; (set-language-environment 'utf-8)
 ;; (prefer-coding-system 'utf-8-unix)
@@ -41,10 +45,11 @@
 ;; Korean settings (from: https://manime.tistory.com/entry/eshell-%EB%B0%8F-dired-%EC%97%90%EC%84%9C-%ED%95%9C%EA%B8%80%EC%9D%B4-%EC%A0%95%EC%83%81%EC%A0%81%EC%9C%BC%EB%A1%9C-%EC%B6%9C%EB%A0%A5%EB%90%98%EB%8F%84%EB%A1%9D-%EC%84%A4%EC%A0%95)
 ;;
 (when enable-multibyte-characters
-  (set-language-environment "Korean")
-  ;; (setq locale-value
-  ;;       (if (string= (getenv "LANG") "ko_KR.utf8") 'utf-8 'euc-kr))
-  (setq locale-value 'utf-8)
+  ;; (set-language-environment "Korean")
+  (set-language-environment "UTF-8")
+  (setq locale-value
+        (if (string= (getenv "LANG") "ko_KR.utf8") 'utf-8 'euc-kr))
+  ;; (setq locale-value 'utf-8)
   (prefer-coding-system locale-value)
   (set-default-coding-systems locale-value)
 
@@ -53,6 +58,15 @@
   (set-terminal-coding-system locale-value)
   (set-keyboard-coding-system locale-value)
   (set-selection-coding-system locale-value))
+
+;; from: https://stackoverflow.com/questions/24904208/emacs-windows-org-mode-encoding
+(modify-coding-system-alist 'file "" 'utf-8-unix)
+
+;; for pasted text (on windows)
+;; from: https://rufflewind.com/2014-07-20/pasting-unicode-in-emacs-on-windows
+(set-selection-coding-system 'utf-16-le)
+;; OR (from https://stackoverflow.com/questions/22647517/emacs-encoding-of-pasted-text)
+;; (set-clipboard-coding-system 'utf-16le')
 
 (when (string-match "^3" (or (getenv "HANGUL_KEYBOARD_TYPE") ""))
   (setq default-korean-keyboard "3")
